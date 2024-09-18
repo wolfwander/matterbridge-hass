2fbf81516219f7968f96ed67c9bf06462ec8f362
wolfwander <wolfwander@hotmail.com>
2024-09-18 11:49:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-import { Matterbridge, PlatformConfig } from 'matterbridge';
-import { AnsiLogger } from 'matterbridge/logger';
-import { ExampleMatterbridgeDynamicPlatform } from './platform.js';
+import { logger } from '@home-assistant-matter-hub/core';
+import type { Matterbridge, PlatformConfig } from 'matterbridge';
+import { AnsiLogger, TimestampFormat } from 'matterbridge/logger';
+import * as ws from 'ws';
 
-/**
- * This is the standard interface for Matterbridge plugins.
- * Each plugin should export a default function that follows this signature.
- *
- * @param {Matterbridge} matterbridge - The Matterbridge instance.
- * @param {AnsiLogger} log - The logger instance.
- * @param {PlatformConfig} config - The platform configuration.
- * @returns {ExampleMatterbridgeDynamicPlatform} The initialized platform.
- */
-export default function initializePlugin(matterbridge: Matterbridge, log: AnsiLogger, config: PlatformConfig): ExampleMatterbridgeDynamicPlatform {
-  return new ExampleMatterbridgeDynamicPlatform(matterbridge, log, config);
+import { NodeAnsiTransport } from './node-ansi-transport.js';
+import { HomeAssistantPlatform } from './platform.js';
+
+const wnd = globalThis as Record<string, unknown>;
+wnd.WebSocket = ws.WebSocket;
+
+if (!Object.prototype.hasOwnProperty.call(AnsiLogger.prototype, 'setGlobalCallback')) {
+  Object.assign(AnsiLogger.prototype, {
+    setGlobalCallback: AnsiLogger.setGlobalCallback.bind(AnsiLogger),
+    getGlobalCallback: AnsiLogger.getGlobalCallback.bind(AnsiLogger),
+  });
+  const patchLogger = new AnsiLogger({
+    logTimestampFormat: TimestampFormat.TIME_MILLIS,
+    logName: 'matterbridge-home-assistant',
+  });
+  patchLogger.warn('Patched NodeAnsiLogger until this PR is released: https://github.com/Luligu/matterbridge/pull/98');
+}
+
+export default function initializePlugin(
+  matterbridge: Matterbridge,
+  ansiLogger: AnsiLogger,
+  platformConfig: PlatformConfig,
+): HomeAssistantPlatform {
+  logger.add(new NodeAnsiTransport(ansiLogger));
+  return new HomeAssistantPlatform(matterbridge, ansiLogger, platformConfig);
 }
