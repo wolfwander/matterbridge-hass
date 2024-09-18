5c7e91ed98cdf1fa9c73ce7e9a271cf31e6e598f
wolfwander <wolfwander@hotmail.com>
2024-09-18 12:51:33 -0300

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -24,7 +24,7 @@ export class HomeAssistantPlatform extends MatterbridgeDynamicPlatform implement
 
   private createDevice(
     vendorName: string,
-  ): (entity: HomeAssistantMatterEntity, definition: DeviceTypeDefinition) => MatterbridgeDevice {
+  ): (entity: HomeAssistantMatterEntity, definition: DeviceTypeDefinition) => MatterDevice {
     return (entity, definition) => {
       const device = new MatterbridgeDevice(definition);
 
